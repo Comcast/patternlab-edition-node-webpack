cf1673c42bd30c098c66a04d269727ed87cb666e
Bulfair, Matt <matt_bulfair@cable.comcast.com>
2018-06-12 10:17:42 -0400

    cleanup for gh-pages branch

--- ISSUE_TEMPLATE.md
+++
@@ -1,7 +0,0 @@
-## I found the following issue:
-
-Describe in as much detail as possible the issue you have discovered.
-
-
-
-### Recommendations on Solutions:
--- source/js/getting-started.js
+++
@@ -1,3 +0,0 @@
-console.info("Welcome to Pattern Library: Webpack Edition");
-console.info("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-console.info("Place any generic javascript files in the ./source/js folder, and webpack will bundle it up for you.");
