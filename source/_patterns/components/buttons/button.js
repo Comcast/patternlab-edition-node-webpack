28119676dd0d427617ff242948fba64f94c7d064
Rey <rey@nothing.ch>
2019-01-30 18:19:02 +0100

    test: add meta head to example

--- source/_patterns/components/buttons/button.js
+++
@@ -1,3 +0,0 @@
-export function init() {
-    console.log('It works!');
-}
--- source/js/main.js
+++
@@ -1,6 +0,0 @@
-import { init } from '../_patterns/components/buttons/button';
-
-function main() {
-    console.log('Init main()');
-    init();
-}
